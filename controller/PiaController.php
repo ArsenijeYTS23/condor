e5a9dc4b26e6258e37cfc24eb5be777dd8d2fda9
ArsenijeYTS23 <36597375+ArsenijeYTS23@users.noreply.github.com>
2020-09-12 17:30:31 +0200

    Add files via upload

---
+++ controller/PiaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Country;
+use App\Usr;
+
+class PiaController extends Controller
+{
+    function user($ipa){
+
+     $file = file('http://pinkman.online/api/?api-key=123');
+  $file =collect(json_decode($file[0], true)['data']);
+
+  $user_transaction = collect($file)->where('user_id',$ipa)->values()->toArray();
+    $usr=Usr::where('id',$ipa)->first()->toArray();
+
+
+  return view('pia',compact('user_transaction','usr'));
+
+    }
+
+    
+   function country(){
+       $user=Usr::get();
+      $country= Country::get();
+         $file = file('http://pinkman.online/api/?api-key=123');
+       $file =collect(json_decode($file[0], true)['data']);
+
+    foreach($country as $count)
+    {
+      $usr=$count->usr->pluck('id');
+      $us_count[]=$file->whereIn('user_id',$usr)->values();
+    }
+
+    foreach($us_count as $count){
+    $amount=   collect($count)->pluck('amount')->sum();
+
+      $usc[]=[collect($count)->count(),$amount];
+    }
+
+//return $usc;
+  return view('country',compact('country','usc'));
+
+   }
+
+
+
+
+
+
+}
